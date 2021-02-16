4519d120b141987f806cbd7e312d39c7da3c3a11
alexandrage <alexandrage@mail.ru>
2021-02-16 18:22:39 +0300

    Add custom autocraft
    Add custom autocraft

--- src/craft/EventListener.java
+++ src/craft/EventListener.java
@@ -1,14 +1,19 @@
 package craft;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.CraftingInventory;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
-
 import com.destroystokyo.paper.event.player.PlayerRecipeBookClickEvent;
 
 public class EventListener implements Listener {
@@ -39,7 +44,7 @@ public class EventListener implements Listener {
 			ItemStack[] cont = inv.getContents();
 			for (int i = 0; i < cont.length; i++) {
 				if (craft.get(i) != null) {
-					if (!craft.get(i).isSimilar(cont[i])) {
+					if (!this.isSimilar(craft.get(i), cont[i])) {
 						e.getInventory().setResult(new ItemStack(Material.AIR));
 						return;
 					}
@@ -50,9 +55,101 @@ public class EventListener implements Listener {
 
 	@EventHandler
 	public void on(PlayerRecipeBookClickEvent e) {
-		if (this.recipe.hasRecipe(e.getRecipe().getKey())) {
-			e.getPlayer().getOpenInventory().getTopInventory();
-			// TODO
+		String key = e.getRecipe().getKey();
+		if (this.recipe.hasRecipe(key)) {
+			e.setCancelled(true);
+			Player player = e.getPlayer();
+			Craft craft = this.recipe.getRecipe(e.getRecipe().getKey());
+			Inventory pinv = player.getInventory();
+			Inventory inv = player.getOpenInventory().getTopInventory();
+			ItemStack[] cont = inv.getContents();
+			List<Integer> list = new ArrayList<Integer>();
+			int min = 0;
+			for (int i = 1; i < cont.length; ++i) {
+				if (craft.get(i) != null) {
+					if (!this.isSimilar(craft.get(i), cont[i])) {
+						inv.setItem(i, new ItemStack(Material.AIR));
+						pinv.addItem(cont[i]);
+						list.add(0);
+					} else {
+						list.add(cont[i].getAmount());
+					}
+				} else if (cont[i] != null || cont[i].getType() != Material.AIR) {
+					inv.setItem(i, new ItemStack(Material.AIR));
+					pinv.addItem(cont[i]);
+				}
+			}
+			Collections.sort(list);
+			if (!list.isEmpty()) {
+				min = list.get(0);
+			}
+			Inventory temp = Bukkit.createInventory(null, 54);
+			temp.setContents(pinv.getContents());
+			for (int i = 1; i < cont.length; ++i) {
+				if ((craft.get(i) != null) && cont[i].getAmount() == min) {
+					if (!this.check(temp, craft.get(i), 1)) {
+						return;
+					}
+				}
+			}
+			cont = inv.getContents();
+			for (int i = 1; i < cont.length; ++i) {
+				if ((craft.get(i) != null) && cont[i].getAmount() == min) {
+					if (this.check(pinv, craft.get(i), 1)) {
+						ItemStack set = craft.get(i);
+						set.setAmount(cont[i].getAmount() + 1);
+						inv.setItem(i, set);
+					}
+				}
+			}
+			cont = null;
+			temp.clear();
 		}
+	}
+
+	public int calc(Inventory inv, ItemStack s) {
+		int count = 0;
+		for (int i = 0; i < inv.getSize(); i++) {
+			ItemStack stack = inv.getItem(i);
+			if (stack == null) {
+				continue;
+			}
+			if (this.isSimilar(stack, s)) {
+				count += stack.getAmount();
+			}
+		}
+		return count;
+	}
+
+	public boolean check(Inventory inv, ItemStack s, int c) {
+		if (this.calc(inv, s) >= c) {
+			this.clear(inv, s, c);
+			return true;
+		}
+		return false;
+	}
+
+	public void clear(Inventory inv, ItemStack s, int c) {
+		for (int i = 0; i < inv.getSize(); i++) {
+			ItemStack stack = inv.getItem(i);
+			if (stack == null)
+				continue;
+			if (this.isSimilar(stack, s)) {
+				if (stack.getAmount() == 0)
+					break;
+				if (stack.getAmount() <= c) {
+					c = c - stack.getAmount();
+					stack.setAmount(-1);
+				}
+				if (stack.getAmount() > c) {
+					stack.setAmount(stack.getAmount() - c);
+					c = 0;
+				}
+			}
+		}
+	}
+
+	public boolean isSimilar(ItemStack s1, ItemStack s2) {
+		return s1.isSimilar(s2);
 	}
 }
